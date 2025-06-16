0acf93adf9c42c8ee256ecb6972f3af602517b18
OleksiiIhnatiev <oleksii.ihnatiev28@gmail.com>
2025-06-16 23:55:30 +0300

    feat(properties): include agent info when fetching all properties

--- client/src/app/api/http/index.ts
+++ client/src/app/api/http/index.ts
@@ -2,44 +2,57 @@ import axios from "axios";
 import { AuthResponse } from "../service/AppService";
 
 export const API_URL = process.env.REACT_APP_API_URL;
-console.log(API_URL)
+console.log(API_URL);
 const $api = axios.create({
-    withCredentials: true,
-    baseURL: API_URL
+  withCredentials: true,
+  baseURL: API_URL,
 });
 
-$api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+$api.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
     return config;
-}, (error) => {
+  },
+  (error) => {
     return Promise.reject(error);
-});
-//* * 
-$api.interceptors.response.use((response) => {
+  }
+);
+//* *
+$api.interceptors.response.use(
+  (response) => {
     return response;
-}, async (error) => {
+  },
+  async (error) => {
     const originalRequest = error.config;
 
-    if (error.response.status === 401 && error.config && !error.config._isRetry) {
-        originalRequest._isRetry = true;
-        try {
-            const response = await axios.get<AuthResponse>(`${API_URL}/token/refresh`, {
-                withCredentials: true, 
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<AuthResponse>(
+          `${API_URL}/token/refresh`,
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-            localStorage.setItem('token', response.data.accessToken);
-            originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        localStorage.setItem("token", response.data.accessToken);
+        originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
 
-            return $api.request(originalRequest);
-        } catch (e) {
-            console.log(e);
-        }
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.log(e);
+      }
     }
 
     return Promise.reject(error);
-});
- 
-export default $api; 
+  }
+);
+
+export default $api;
